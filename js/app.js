4f71e2d12773e1c75891e1abadb94bdbc6cde867
marcelolaba <63062775+marcelolaba@users.noreply.github.com>
2020-07-11 14:38:17 -0300

    intern links

--- js/app.js
+++ js/app.js
@@ -30,26 +30,3 @@ window.addEventListener("scroll", function () {
     homebtn.classList.remove("show-button");
   }
 });
-
-// ********** smooth scroll ************
-// select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    // prevent default
-    e.preventDefault();
-    links.classList.remove("show-links");
-
-    const id = link.getAttribute("href").slice(1);
-    const element = document.getElementById(id);
-    //
-    let position = element.offsetTop - 62;
-
-    window.scrollTo({
-      left: 0,
-      // top: element.offsetTop,
-      top: position,
-      behavior: "smooth",
-    });
-  });
-});
